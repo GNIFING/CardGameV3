93f8b682f8c0db9047bc392796935d7756e0eb9e
GNIFING <narawich.fing@gmail.com>
2023-05-25 16:38:37 +0700

    UO 19 new student

---
+++ Assets/Scripts/UnitSkills/Ogre/UO19.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UO19 : UnitCard
+{
+    void Start()
+    {
+        InitializeCardStats();
+        UpdateCardUI();
+    }
+    public override void UnitSkill()
+    {
+        Debug.Log("Ogre 19 Skill !");
+        isSkillDone = true;
+    }
+    public override void UnitHighlight()
+    {
+        isSkillDone = true;
+        tileManager.NoHighlightUnit();
+        Debug.Log("Highlight from Ogre 19");
+    }
+}
